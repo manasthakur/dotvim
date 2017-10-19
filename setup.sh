c44949516976daa7b84bc4dd583f3fc8c79a997c
Manas Thakur <manas@pikachu>
2017-10-19 21:54:23 +0530

    vim-commentor is back as a plugin.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ mkdir -p pack/mine/start pack/mine/opt pack/others/start pack/others/opt .sessio
 
 cd pack/mine/start/
 git clone https://github.com/manasthakur/vim-minisnip.git
+git clone https://github.com/manasthakur/vim-commentor.git
 
 cd ../opt/
 git clone https://github.com/manasthakur/vim-asyncmake.git
