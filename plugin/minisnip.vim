d8b55c57f1cbebd8608c38ff10d11a56558e6e1d
Manas <manas@iMac>
2018-05-11 13:25:35 +0530

    Switched to joereynolds's minisnip plugin.

---
+++ plugin/minisnip.vim
@@ -0,0 +1,9 @@
+let g:minisnip_dir = '~/.vim/snippets'
+
+let g:minisnip_trigger = '<C-j>'
+
+let g:minisnip_startdelim = '<+'
+let g:minisnip_enddelim = '+>'
+
+let g:minisnip_finalstartdelim = '<-'
+let g:minisnip_finalenddelim = '->'
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 mkdir -p pack/mine/start pack/mine/opt pack/others/start pack/others/opt .sessions .swap
 
 cd pack/mine/start/
-git clone https://github.com/manasthakur/vim-minisnip.git
 git clone https://github.com/manasthakur/vim-commentor.git
 
 cd ../opt/
@@ -10,6 +9,11 @@ git clone https://github.com/manasthakur/vim-asyncmake.git
 cd ../../others/start/
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/joereynolds/vim-minisnip.git
+
+cd ../opt/
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/romainl/vim-qf
 
 cd ~
 ln -s ~/.vim/vimrc .vimrc
